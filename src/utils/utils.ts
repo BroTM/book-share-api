b50e5cafcc48d39884fbedbff8c56e25cd72bd91
BroTM <thanmyat.sd@gmail.com>
2023-10-01 00:27:27 +0630

    main: custom validation for no special character and fix_length 8

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,27 +1,51 @@
 class Utils {
-    hasValNotEmpty(obj: any, key: any) {
-        if (obj === null) return false;
-        if (typeof obj !== 'object') return false;
-    
-        if (key === null) return false;
-        if (typeof key === 'undefined') return false;
-        if (typeof key === 'string' && key === '') return false;
-    
-        if (obj[key] === null) return false;
-        if (typeof obj[key] === 'undefined') return false;
-        if (typeof obj[key] === 'string' && obj[key] === '') return false;
-        if (typeof obj[key] === 'number' && obj[key] === 0) return false;
-    
-        return true;
-      }
+  constructor() { }
+  public static hasValNotEmpty(obj: any, key: any) {
+    if (obj === null) return false;
+    if (typeof obj !== 'object') return false;
 
-      isNumber(value: any) : Boolean {
-        return (typeof value == "number" || typeof value == "string" && value.match(/^[0-9]+$/))? true: false;
-      }
+    if (key === null) return false;
+    if (typeof key === 'undefined') return false;
+    if (typeof key === 'string' && key === '') return false;
 
-      isUUid(value: any) : Boolean {
-        return typeof value == "string" && value.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i) ? true: false;
-      }
+    if (obj[key] === null) return false;
+    if (typeof obj[key] === 'undefined') return false;
+    if (typeof obj[key] === 'string' && obj[key] === '') return false;
+    if (typeof obj[key] === 'number' && obj[key] === 0) return false;
+
+    return true;
+  }
+
+  public static isNumber(value: any): Boolean {
+    return (typeof value == "number" || typeof value == "string" && value.match(/^[0-9]+$/)) ? true : false;
+  }
+
+  public static isUUid(value: any): Boolean {
+    return typeof value == "string" && value.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i) ? true : false;
+  }
+
+  /**
+   * validator
+   * length must be 8
+   * no special character allowed
+   * 
+   * @param value 
+   * @returns Boolean
+   */
+  public static isNoSpecialWithLenght8(value: string): Boolean {
+    return (typeof value === "string" && value.length == 8 && value.match(/^[a-zA-Z0-9]+$/i)) ? true : false;
+  }
+
+  public static validateDateFormat(value: any) {
+    value = Utils.isNumber(value) ? parseInt(value) : value;
+
+    if (typeof value !== "number")
+      throw new Error('Incorrect Date Value!');
+
+    let date: any = new Date(value);
+    if (date == "Invalid Date")
+      throw new Error('Incorrect Date Value!');
+  }
 }
 
-export default new Utils();
+export default Utils;
